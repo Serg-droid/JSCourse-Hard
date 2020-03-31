a61d1d374c1e41931cf8d7d8604fb15468da49c1
Сергей Худяков <sergei.hudakov@gmail.com>
2020-04-01 00:19:22 +0300

    Lesson 04

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-let lang;
-lang = prompt('Write a lang:');
-let daysOfWeekRu = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
-let daysOfWeekEn = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
-
-// 1 ----->
-
-// a
-if (lang === 'ru') {
-	console.log(daysOfWeekRu);
-} else {
-	console.log(daysOfWeekEn);
-}
-// b
-switch (lang) {
-	case 'en':
-		console.log(daysOfWeekEn);
-		break;
-	default:
-		console.log(daysOfWeekRu);
-}
-// c
-lang === 'ru' ? console.log(daysOfWeekRu):console.log(daysOfWeekEn);
-// d (то, о чем я писал в дискорде)
-daysOfWeek = {
-	ru: daysOfWeekRu,
-	en: daysOfWeekEn,
-}
-console.log(daysOfWeek[lang]);
-
-// 2 ----->
-
-let namePerson = prompt('Put a name: ');
-
-namePerson === 'Артем' ? console.log('директор'):namePerson === 'Максим' ? console.log('преподаватель'):console.log('студент');
-
-
-
-
+let checkInput = function(symbols) {
+	if (typeof symbols !== 'string') {
+		alert('It is not a string');
+		return;
+	} else {
+		let trimmedString = symbols.trim();
+		if (symbols.length > 30) {
+			return trimmedString.slice(0,31) + '...';
+		} 
+		return trimmedString;
+	}
+};
 
 
