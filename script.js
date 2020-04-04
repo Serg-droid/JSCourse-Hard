69c4e9053137e63f0eee6f685e4723ac68824195
Serg-droid <61561342+Serg-droid@users.noreply.github.com>
2020-04-04 16:02:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday', 'Saturday'];
+const week = ['Tuesday', 'Monday', 'Wednesday', 'Thursday', 'Friday', 'Sunday', 'Saturday'];
 
-let today = new Date();
+let day = new Date();
 
-for (let i = 0; i < week.length; i++) {
-	i === today.getDate() + 1 || (i === 6 && today.getDate() === 0) ? console.log(week[i].bold()) : i === 5 || i === 6 ? console.log(week[i].italics()) : console.log(week[i])
+const checkDay = (thisDay) => {
+	return thisDay === 0 ? 'Saturday' : thisDay === 1 ? 'Monday' : thisDay === 2 ? 'Tuesday' : thisDay === 3 ? 'Wednesday' : thisDay === 4 ? 'Thursday' : thisDay === 5 ? 'Friday' : 'Sunday'
+};
+const today = checkDay(day);
+
+for (const day of week) {
+	const p = document.createElement('p');
+	if (day === today) {
+		p.innerHTML = `<b>${day}</b>`;
+		document.body.append(p);
+	} else if (day === 'Saturday' || day ==='Sunday') {
+		p.innerHTML = `<i>${day}</i>`;
+		document.body.append(p);
+	} else {
+		p.innerHTML = `${day}`;
+		document.body.append(p);
+	}
 }
 
